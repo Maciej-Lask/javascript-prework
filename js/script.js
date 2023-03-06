1524073f8ad4de018b61561057fd5558c245a738
Maciej Laskowski <maciek30088@gmail.com>
2023-03-06 18:22:40 +0100

    Add playerMove variable to js/script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let computerMove = `kamieñ`;
+let playerMove = 'papier'
+printMessage('Zagra³em ' + computerMove + '! Jeœli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagra³em ' + playerMove + '! Wygrywam!');
